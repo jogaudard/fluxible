e4b30e3da49f2d071f4894423f83b4bc564a2c60
jogaudard <joseph.gaudard@pm.me>
2023-07-28 09:43:23 +0200

    testing functions

---
+++ code/example.R
@@ -0,0 +1,25 @@
+# this script will be to test the functions, and will also serve a an example of the workflow with this package
+
+
+# packages and sources ----------------------------------------------------
+
+
+
+# import data -------------------------------------------------------------
+
+
+# match data --------------------------------------------------------------
+
+
+# fitting the fluxes ------------------------------------------------------
+
+
+# graphs and visual assessment --------------------------------------------
+
+
+# quality assessment ------------------------------------------------------
+
+
+# calculation of fluxes ---------------------------------------------------
+
+
