87bfc0f2b60a7be322808577e97fe492a4ee3602
jogaudard <joseph.gaudard@pm.me>
2023-10-19 17:29:20 +0200

    started working on tests

--- R/building.R
+++ R/building.R
@@ -7,7 +7,10 @@ library(available)
 
 # finding out if the package name is available ----------------------------
 
-available("wtflux")
+# wtflux sounds too offensive to native English speakers
+# other ideas: fluxury, fluxhurry, fluxable, fluxible
+
+available("fluxhurry")
 
 
 # creating the structure --------------------------------------------------
@@ -21,27 +24,78 @@ available("wtflux")
 # tests to check the package ----------------------------------------------
 
 # what do I need to test?
-#
+# should I create a fake dataset for which I know the values to test those functions? yes (Richard)
+# advice from Richard: run the function on a dataset in the beginning when I am sure of what it is doing, then use this as a test. It will test if the function behaviour ahs changed.
+# what about having functions dealing ONLY with fluxes, and not dragging along PAR, temperatures and co values?
+
+usethis::use_testthat()
+
+# creating a sample dataset to use in the test
+# we can use PFTC6 data because that way we have data over midnight (special case)
+
+library(dataDownloader)
+library(tidyverse)
+
+get_file(node = "fcbw4",
+         file = "PFTC6_CO2_joasete_2022.csv",
+         path = "data",
+         remote_path = "raw_data/c_flux_raw_data")
+
+get_file(node = "fcbw4",
+         file = "PFTC6_cflux_field-record_joasete.csv",
+         path = "data",
+         remote_path = "raw_data/c_flux_raw_data")
+
+co2_df <- read_csv("data/PFTC6_CO2_joasete_2022.csv")
+record <- read_csv("data/PFTC6_cflux_field-record_joasete.csv")
+
+# we want a shorter standard dataset covering midnight
+co2_df_short <- co2_df %>%
+   rename(
+    datetime = "Date/Time",
+    temp_air = "Temp_air ('C)",
+    temp_soil = "Temp_soil ('C)",
+    CO2 = "CO2 (ppm)",
+    PAR = "PAR (umolsm2)"
+   ) %>%
+      select(datetime, temp_air, temp_soil, CO2, PAR) %>% # we keep just the variables we need
+         filter( # we will just make it shorter and keep a couple of fluxes around midnight
+                datetime >= 
+         )
+
+
+
+# a dataset with too many missing data
+
+
 
 
 # package workflow --------------------------------------------------------
 # it seems that to make testing easier I need to split my functions in smaller bits (I can add some wrap-up functions later)
 #
-# match.flux: will stay one function.
+# what.ID: match fluxes with measurement ID
+# will stay one function.
 # this function matches continuously logged data into the different measurements and adds a measurement ID.
 # it does so by using the field record provided by the user (record of which measurements was taken when with same time as the logger)
 # side-note: we could include a time difference parameter in case the logger and the user were not synchronized
 
 # fitting.fluxes: this one will be split into several functions
-# slope.flux: a function to calculate the slope of the flux
-# quality.flux: to calculate the quality parameters of each flux (RMSE, NRMSE, correlation coefficient and co)
-# flag.flux: to add the flags based on the quality parameters and the user's choices
+# what.slope: a function to calculate the slope of the flux
+# what.quality : to calculate the quality parameters of each flux (RMSE, NRMSE, correlation coefficient and co)
+# what.flag: to add the flags based on the quality parameters and the user's choices
+
 
 
 # should I include a function to graph the fluxes or can we assume that people know how to use ggplot?
 
-# calculate.flux: this function is only calculating the fluxes based on the slope and other needed inputs
+# what.flux: this function is only calculating the fluxes based on the slope and other needed inputs
 # GPP and corrections will be other functions
+
+# what.GPP: to calculate GPP as the difference between NEE and ER
+
+# what.GPP_corr: to correct GPP for nighttime measurement (stable layer issue, specific to PFTC6 measurements)
+
+
 
 
 
---
+++ tests/testthat/test-flux_match.R
@@ -0,0 +1,14 @@
+# what do we need to test with match?
+# standard sample of data
+test_that("matching works", {
+  ### setup
+  co2_df <- 
+  )
+  expect_equal(2 * 2, 4)
+})
+
+# special case when flux is over midnight (change in date): this can be included in the standard match
+# not enough data within the window provided returns a flag
+# 
+
+
