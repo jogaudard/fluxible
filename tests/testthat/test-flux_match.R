410d3c525cab5408452ce83996a187b1267d012c
jogaudard <joseph.gaudard@pm.me>
2023-10-27 18:04:08 +0200

    minor changes in test match

--- tests/testthat/test-flux_match.R
+++ tests/testthat/test-flux_match.R
@@ -2,9 +2,9 @@
 # standard sample of data
 test_that("matching works", {
   ### setup
-  co2_df_short <- read_csv("data/co2_df_short.csv")
-  record_short <- read_csv("data/co2_df_short.csv")
-  co2_conc <- read_csv("data/co2_conc.csv")
+  co2_df_short <- readr::read_csv("data/co2_df_short.csv")
+  record_short <- readr::read_csv("data/co2_df_short.csv")
+  co2_conc <- readr::read_csv("data/co2_conc.csv")
 
 
   
