2d9b15e0c29d5f8f7d39d88f5d02aaceb4cb4101
jogaudard <joseph.gaudard@pm.me>
2024-03-04 16:42:31 +0100

    errors in quality exp

--- R/flux_quality_exp.R
+++ R/flux_quality_exp.R
@@ -1,6 +1,36 @@
 #' assessing fluxes quality calculated with an exponential fit
+#' quality assessment for the fluxes calculated with the exponential model
+#' @description indicates if fluxes should be discarded or replaced by 0 according to parameters set by user
+#' @param slopes_df dataset containing slopes, fluxID, and parameters of the exponential expression
+#' @param fluxID_col column containing unique IDs for each flux
+#' @param conc_col column containing the measured gas concentration
+#' @param b_col column containing the b parameter of the exponential expression
+#' @param time_col column containing the time of each measurement in seconds
+#' @param fit_col column containing the modeled data
+#' @param slope_col column containing the slope of the exponential expression used for the calculation
+#' @param weird_fluxesID vector of fluxIDs that should be discarded by the user's decision
+#' @param RMSE_threshold threshold for the RMSE of each flux above which the fit is considered unsatisfactory
+#' @param cor_threshold threshold for the correlation coefficient of gas concentration with time below which the correlation is considered non significant
+#' @param b_threshold threshold for the b parameter. Defines a window with its opposite inside which the fit is considered good enough.
+#' @return same dataframe with added flag and corrected slopes columns
+#' @importFrom dplyr mutate case_when rename group_by rowwise summarise ungroup
+#' @importFrom tidyr nest unnest
+#' @importFrom stats cor
+#' @examples 
+#' data(slopes0)
+#' flux_quality_exp(slopes0,
+#' fluxID_col = "fluxID",
+#' conc_col = "conc",
+#' b_col = "b",
+#' time_col = "time",
+#' fit_col = "fit",
+#' slope_col = "slope_tz"
+#' )
+#' @export 
 #' 
 #' 
+
+# need to check time vs time_cut, we want the quality assessment only on the part that we keep
 
 flux_quality_exp <- function(slopes_df,
                             fluxID_col = "f_fluxID",
@@ -17,7 +47,7 @@ flux_quality_exp <- function(slopes_df,
                             error = 100 # error of the setup in ppm. fluxes starting outside of the window ambient_CO2 +/- error will be discarded
 ){
 
-    fluxes_df <- fluxes_df |>
+    slopes_df <- slopes_df |>
         rename(
             f_fluxID = all_of((fluxID_col)),
             f_conc = all_of((conc_col)),
@@ -28,8 +58,8 @@ flux_quality_exp <- function(slopes_df,
         )
 
     
-    quality_par <- fluxes_df |>
-        group_by(.data$fluxID) |>
+    quality_par <- slopes_df |>
+        group_by(.data$f_fluxID) |>
         nest() |>
         rowwise() |>
         summarise(
@@ -61,10 +91,10 @@ flux_quality_exp <- function(slopes_df,
         .data$f_fit_quality == "bad_RMSE" & .data$f_correlation == "no" ~ "zero"
       ),
       f_slope_corr = case_when(
-        .data$f_flag == "weird_flux" ~ NA_real_,
-        .data$f_flag == "start_error" ~ NA_real_,
-        .data$f_flag == "discard" ~ NA_real_,
-        .data$f_flag == "zero" ~ 0,
+        .data$f_flag_fit == "weird_flux" ~ NA_real_,
+        .data$f_flag_fit == "start_error" ~ NA_real_,
+        .data$f_flag_fit == "discard" ~ NA_real_,
+        .data$f_flag_fit == "zero" ~ 0,
         TRUE ~ .data$f_slope_tz,
       )
     )
--- R/flux_quality_lin.R
+++ R/flux_quality_lin.R
@@ -1,40 +1,54 @@
-#' to provide quality assessment for the fluxes calculated with the linear model
+#' quality assessment for the fluxes calculated with the linear model
+#' @description indicates if fluxes should be discarded or replaced by 0 according to parameters set by user
+#' @param slopes_df dataset containing slopes, fluxID, p.value and r.squared
+#' @param pvalue_threshold threshold of p-value below which the change of gas concentration over time is considered not significant (user decided)
+#' @param rsquared_threshold threshold of r squared value below which the linear model is considered an unsatisfactory fit
+#' @param fluxID_col column containing unique IDs for each flux
+#' @param slope_col column containing the slope of each flux (ideally as calculated by the flux_fitting function)
+#' @param pvalue_col column containing the p-value of each flux
+#' @param rsquared_col column containing the r squared to be used for the quality assessment
+#' @param ambient_CO2 ambient CO2 concentration in ppm at the site of measurement (used to detect measurement that started with a polluted setup)
+#' @param error error of the setup, defines a window in which the starting values are considered acceptable
+#' @return same dataframe with added flag and corrected slopes columns
+#' @importFrom dplyr mutate case_when rename
+#' @examples 
+#' data(slopes0lin)
+#' flux_quality_lin(slopes0lin, fluxID_col = "fluxID", slope_col = "slope")
+#' @export 
 
 
-flux_quality_lin <- function(fluxes_df,
+flux_quality_lin <- function(slopes_df,
                             pvalue_threshold = 0.3,
                             rsquared_threshold = 0.7,
-                            blind_replace = FALSE, # if TRUE, it replaces the data by NA or 0 according to the quality flags
-                            fluxID_col = "fluxID",
-                            fluxes_col = "fluxes",
+                            fluxID_col = "f_fluxID",
+                            slope_col = "f_slope",
                             pvalue_col = "p.value",
                             rsquared_col = "r.squared",
                             ambient_CO2 = 421,
                             error = 100 # error of the setup in ppm. fluxes starting outside of the window ambient_CO2 +/- error will be discarded
 ){
 
-    fluxes_df <- fluxes_df |>
+    slopes_df <- slopes_df |>
         rename(
-            fluxID = all_of((fluxID_col)),
-            fluxes = all_of((fluxes_col)),
-            pvalue = all_of((pvalue_col)),
-            rsquared = all_of((rsquared_col))
+            f_fluxID = all_of((fluxID_col)),
+            f_slope = all_of((slope_col)),
+            f_pvalue = all_of((pvalue_col)),
+            f_rsquared = all_of((rsquared_col))
         )
 
-    fluxes_df |>
+    slopes_df |>
         mutate(
-            quality_flag = case_when(
-                .data$rsquared >= ((rsquared_threshold)) ~ "ok",
-                .data$rsquared < ((rsquared_threshold)) & .data$pvalue >= ((pvalue_threshold)) ~ "discard",
-                .data$rsquared < ((rsquared_threshold)) & .data$pvalue < ((pvalue_threshold)) ~ "zero"
+            f_quality_flag = case_when(
+                .data$f_rsquared >= ((rsquared_threshold)) ~ "ok",
+                .data$f_rsquared < ((rsquared_threshold)) & .data$f_pvalue >= ((pvalue_threshold)) ~ "discard",
+                .data$f_rsquared < ((rsquared_threshold)) & .data$f_pvalue < ((pvalue_threshold)) ~ "zero"
             ),
-            fluxes = case_when(
-                ((blind_replace)) == FALSE ~ .data$fluxes,
-                ((blind_replace)) == TRUE ~ case_when(
-                    .data$quality_flag == "ok" ~ .data$fluxes,
-                    .data$quality_flag == "discard" ~ NA_real_,
-                    .data$quality_flag == "zero" ~ 0
-                )
+            f_slope_corr = case_when(
+                    .data$f_quality_flag == "ok" ~ .data$f_slope,
+                    .data$f_quality_flag == "discard" ~ NA_real_,
+                    .data$f_quality_flag == "zero" ~ 0
             )
         )
+
+    slopes_df
 }
