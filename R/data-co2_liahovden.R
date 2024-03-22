47870398ea78009a2b530f6cd642875391c57ada
jogaudard <joseph.gaudard@pm.me>
2024-03-22 16:34:44 +0100

    documentation for example data

---
+++ R/data-co2_liahovden.R
@@ -0,0 +1,15 @@
+#' CO2 concentration at Liahovden
+#'
+#' CO2 concentration at Liahovden site, used in example in reamdme file
+#'
+#' @format A tibble with 89692 rows and 5 variables
+#' \describe{
+#' \item{datetime}{Datetime at which CO2 concentration was recorded.}
+#' \item{temp_air}{Air temperature inside the flux chamber in Celsius.}
+#' \item{temp_soil}{Ground temperature inside the flux chamber in Celsius.}
+#' \item{conc}{CO2 concentration in ppm.}
+#' \item{PAR}{Photosyntethically active radiation inside the chamber in micromol/s/sqm.}
+#' }
+#' @examples
+#' co2_liahovden
+"co2_liahovden"
