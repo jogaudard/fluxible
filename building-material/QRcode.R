904cc71caa8a71d4378aead3f10cca7031ae7f5e
jogaudard <joseph.gaudard@pm.me>
2024-12-16 13:24:06 +0100

    webpage in QR code changed to PFTC repo

--- building-material/QRcode.R
+++ building-material/QRcode.R
@@ -1,4 +1,4 @@
 library(qrcode)
-f_qrcode <- qr_code("https://jogaudard.github.io/fluxible/")
+f_qrcode <- qr_code("https://plant-functional-trait-course.github.io/fluxible/")
 plot(f_qrcode)
 generate_svg(f_qrcode, filename = "man/figures/f_qrcode.svg")
