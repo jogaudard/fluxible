233ea03b10d6bbe0f8c1176ae36c390a2dbef5e7
jogaudard <joseph.gaudard@pm.me>
2025-02-07 15:45:57 +0100

    documentation terra data

---
+++ R/data-raw_terra.R
@@ -0,0 +1,14 @@
+#' CO2 and CH4 concentration
+#'
+#' CO2 and CH4 measured simultaneoulsy
+#'
+#' @format A tibble with 21681 rows and 4 variables
+#' \describe{
+#' \item{co2_conc}{CO2 concentration in ppm}
+#' \item{ch4_conc}{CH4 concentration in ppb}
+#' \item{datetime}{Datetime on the datapoint}
+#' \item{temp_air}{Air temperature inside the chamber in Celsius}
+#' }
+#' @examples
+#' raw_terra
+"raw_terra"
