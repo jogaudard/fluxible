ff4300da7a663a387c2b6e4f0c5370136ea9b071
jogaudard <joseph.gaudard@pm.me>
2025-06-12 14:29:00 +0200

    need to make data for test and example

--- R/flux_lrc.R
+++ R/flux_lrc.R
@@ -1,53 +1,91 @@
+#' Corrects CO2 fluxes at fixed PAR values
+#' @description Calculates light response curves for CO2 fluxes
+#' @param fluxes_df a dataframe containing NEE, ER and LRC measurements
+#' @param type_col column containing type of flux (NEE, ER, LRC)
+#' @param par_ave column containing the PAR value for each flux
+#' @param f_flux column containing flux values
+#' @param nee_arg argument designating NEE fluxes in type column
+#' @param er_arg argument designating ER fluxes in type column
+#' @param lrc_arg argument designating LRC fluxes in type column
+#' @param lrc_group character vector of columns to use to group the LRC
+#' (campaign, site, treatment), if applicable
+#' @param par_nee PAR value to correct the NEE fluxes to
+#' @param par_er PAR value to correct the ER fluxes to
+#' @details The light response curves are calculated with a quadratic of the
+#' form
+#' \ifelse{html}{\out{flux ~ PAR + PAR<sup>2</sup>}}{\eqn{flux \~ PAR + PAR^2}{ASCII}}
+#' @return the same dataframe with the additional column `PAR_corrected_flux`
+#' @importFrom dplyr group_by_at filter rename vars select mutate left_join
+#' case_when
+#' @importFrom tidyr nest unnest
+#' @importFrom purrr map
+#' @importFrom broom tidy
+#' @examples 
+#' data(co2_fluxes_lrc)
+#' flux_lrc(
+#' fluxes_df = co2_fluxes,
+#' type_col = type,
+#' par_ave = PAR,
+#' f_flux = f_flux,
+#' lrc_arg = "LRC",
+#' nee_arg = "NEE",
+#' er_arg = "ER",
+#' lrc_group = c(),
+#' par_nee = 300,
+#' par_er = 0
+#' )
+#' @export 
 
 
 flux_lrc <- function(fluxes_df,
-                    flux_col = "fluxes",
-                    type_col = "type",
+                    type_col,
+                    par_ave = {{par_ave}},
+                    f_flux = {{f_flux}},
                     lrc_arg = "LRC",
                     nee_arg = "NEE",
                     er_arg = "ER",
                     lrc_group = c(),
-                    PARfix = 300,
-                    PARnull = 0
-                    ){
+                    par_nee = 300,
+                    par_er = 0) {
 
-fluxes_df <- fluxes_df |>
-  rename(
-    fluxes = all_of(((flux_col))),
-    type = all_of(((type_col)))
-  )
 
-  coefficients_lrc <- fluxes_df %>%
+  coefficients_lrc <- fluxes_df |>
     filter(
-        type == ((lrc_arg))
+        {{type_col}} == ((lrc_arg))
+    ) |>
+    rename(
+      PARavg = {{par_ave}},
+      f_flux = {{f_flux}}
     ) |>
     group_by_at(vars(all_of(((lrc_group))))) |>
-    nest %>% 
-    mutate(lm = map(data, ~ lm(flux ~ PARavg + I(PARavg^2), data = .x)),
+    nest() |>
+    mutate(lm = map(data, ~ lm(f_flux ~ PARavg + I(PARavg^2), data = .x)),
            table = map(lm, tidy),
-           table = map(table, select, term, estimate),
-           table = map(table, pivot_wider, names_from = term, values_from = estimate)
+           table = map(table, select, "term", "estimate"),
+           table = map(table, pivot_wider, names_from = "term", values_from = "estimate")
            
-    ) %>% 
-    unnest(table) %>% 
-    select(all_of(group), `(Intercept)`, PARavg, `I(PARavg^2)`) %>% 
+    ) |>
+    unnest(table) |>
+    select(all_of(group), PARavg, `I(PARavg^2)`) |>
     rename(
       origin = "(Intercept)",
       a = "I(PARavg^2)",
       b = "PARavg"
     )
   
-  flux_corrected_PAR <- fluxes_df %>% 
+  flux_corrected_PAR <- fluxes_df |>
     filter(
-      type %in% c("NEE", "ER")
-    ) %>% 
-    left_join(coefficients_lrc) %>% 
+      {{type_col}} %in% c("NEE", "ER")
+    ) |>
+    left_join(coefficients_lrc) |>
     mutate(
       PAR_corrected_flux = 
         case_when( #we correct only the NEE
-          type == "NEE" ~ flux + a * (PARfix^2 - PARavg^2) + b * (PARfix - PARavg),
-          type == "ER" ~ flux + a * (PARnull^2 - PARavg^2) + b * (PARnull - PARavg)
+          type == "NEE" ~ {{f_flux}} + a * (par_nee^2 - {{par_ave}}^2) + b * (par_nee - {{par_ave}}),
+          type == "ER" ~ {{f_flux}} + a * (par_er^2 - {{par_ave}}^2) + b * (par_er - {{par_ave}})
         )
-    ) %>% 
-    select(!c(a, b, origin))
+    ) |>
+    select(!c("a", "b"))
+  
+  flux_corrected_PAR
 }
