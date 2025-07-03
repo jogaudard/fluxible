f73ecd236a5b17eacfec605804e70f516fdda5cf
jogaudard <joseph.gaudard@pm.me>
2025-07-03 17:03:16 +0200

    par correction now TRUE or NA

--- R/flux_lrc.R
+++ R/flux_lrc.R
@@ -15,8 +15,8 @@
 #' @details The light response curves are calculated with a quadratic of the
 #' form
 #' \ifelse{html}{\out{flux(PAR) = a * PAR<sup>2</sup> + b * PAR + c}}{\eqn{flux(PAR) = a * PAR^2 + b * PAR + c}{ASCII}}
-#' @return the same dataframe with the additional column `par_correction`
-#' indicating `par_corrected` for correct fluxes. Corrected fluxes
+#' @return the same dataframe with the additional column `par_correction = TRUE`
+#' for correct fluxes. Corrected fluxes
 #' are in the same `f_flux` column. Non corrected fluxes and other fluxes are
 #' kept, with NA in `par_correction`.
 #' @details The long format of the output with both uncorrected and corrected
@@ -133,7 +133,7 @@ flux_lrc <- function(fluxes_df,
               a * (par_er^2 - {{par_ave}}^2) +
               b * (par_er - {{par_ave}})
         ),
-      par_correction = "par_corrected"
+      par_correction = TRUE
     ) |>
     bind_rows(fluxes_df) |>
     arrange(.data$rowid) |>
