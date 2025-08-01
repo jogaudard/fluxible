f905ac22f09a60d9997f21b45b6f84db7f33b85b
jogaudard <joseph.gaudard@pm.me>
2025-08-01 15:35:10 +0200

    function. now needs testing and example

---
+++ R/flux_drygas.R
@@ -0,0 +1,30 @@
+#' wet air correction
+#' @descripton Corrects for the amount of water vapor inside the air
+#' @param conc_df dataframe of gas concentration over time
+#' @param gas_wet the gas to correct (in ppm)
+#' @param h2o_wet water vapor concentration before correction (in ppt)
+#' @return the same dataframe with the additional column `[gas_wet]_dry`
+#' @details the correction is done as follows
+#' \ifelse{html}{\out{gas_dry = gas_wet / (1 - (h2o_wet / 1000))}}{\eqn{gas_dry = gas_wet / (1 - (h2o_wet / 1000}{ASCII}}
+#' @importFrom rlang enquo as_name !! :=
+#' @importFrom dplyr mutate
+#' @export
+#' @examples
+
+
+flux_drygas <- function(conc_df,
+                        gas_wet,
+                        h2o_wet) {
+
+  gas_wet <- enquo(gas_wet)
+  h2o_wet <- enquo(h2o_wet)
+
+  gas_dry_name <- paste0(as_name(gas_wet), "_dry")
+
+  output <- conc_df |>
+    mutate(
+      !!gas_dry_name := (!!gas_wet) / (1 - (!!h2o_wet / 1000))
+    )
+
+  output
+}
